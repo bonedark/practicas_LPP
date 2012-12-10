26a6c365951e69ba16616bbd851ceb2a2759e2a3
Daniel Nicolás Fernández del Castillo Salazar <bonedark@gmail.com>
2012-12-10 15:49:25 +0000

    practica 5
    p5

---
+++ practica8/spec/matrix_spec.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+require 'matrix'
+
+describe Matrix do
+
+	before :each do
+		@m = Matrix.new()	
+	end
+
+
+	it "Se debe poder sumar dos matrices de enteros" do
+	end
+
+	it "Se debe poder sumar dos matrices de racionales" do
+	end
+
+	it "Se debe poder multiplicar una matriz de Enteros" do
+	end
+
+	it "Se debe poder multiplicar dos matrices de Racionales" do
+	end
+
+end
+
+
